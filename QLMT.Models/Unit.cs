2f1d8d958656c52bfbf0e9360ae9677e1eb94e8c
vietpt21 <vietvpvbhp@gmail.com>
2024-06-04 17:55:31 +0700

    update

---
+++ QLMT.Models/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace QLMT.Models
+{
+    public class Unit
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int UnitId { get; set; }
+        public string UnitName { get; set; }
+        public string Note { get; set; }
+        [Required]
+        public int LineId { get; set; }
+      
+    }
+}
